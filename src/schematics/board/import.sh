7c30739aa43e85ab911fa8275a85e618684726b7
Andrea Zoppi <texzk@email.it>
2012-03-09 20:23:37 +0100

    Regulator datasheet and WIP PCB drafts
    Added Microchip MCP1700 voltage regulator datasheet.
    
    Added work-in-progress Eagle schematics and PCB drawings for both the
    main board and the sensor module. The sensor PCB should be already
    correct.
    
    Andrea Zoppi

---
+++ src/schematics/board/import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+project_folder=board						# Matches the Eagle subfolder name
+
+eagle_folder=UNKNOWN
+this_folder=$(pwd)
+platform=$(uname -a)
+if [ "x${platform:0:5}" = "xMINGW" ]; then
+	eagle_folder=$HOME/Documents/eagle		# Windows
+else
+	eagle_folder=$HOME/eagle				# Linux (default)
+fi
+
+cd "$eagle_folder/$project_folder"
+files=$(ls -Q1 | grep -v '.*\..#.')
+echo "$files" | xargs -i cp -Rv {} "$this_folder"
+cd "$this_folder"
