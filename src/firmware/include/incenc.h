eb279e8b5d932c82c9370a0cf58a842bf1e4fa6d
Andrea Zoppi <texzk@email.it>
2012-03-16 11:15:23 +0100

    Added incremental encoder template code
    An incremental encoder software module was added to the firmware project.
    It is not implemented yet.
    
    Andrea Zoppi

---
+++ src/firmware/include/incenc.h
@@ -0,0 +1,83 @@
+/**
+ * Incremental encoder driver.
+ *
+ * This file contains the definitions to handle the incremental encoder.
+ *
+ * @author Andrea "TexZK" Zoppi
+ *
+ */
+
+#ifndef __INCENC_H__
+#define	__INCENC_H__
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// HEADERS
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// GLOBAL DEFINITIONS
+
+// Pins configuration
+#define	INCENC_TRIS_A			TRISCbits.TRISC1	/// Signal A tristate
+#define	INCENC_TRIS_B			TRISCbits.TRISC0	/// Signal B tristate
+
+#define	INCENC_PIN_A			PORTCbits.RC1		/// Signal A pin
+#define	INCENC_PIN_B			PORTCbits.RC0		/// Signal B pin
+
+#define	INCENC_LAT_A			LATCbits.LATC1		/// Signal A latch
+#define	INCENC_LAT_B			LATCbits.LATC0		/// Signal B latch
+
+#define	INCENC_ANS_A			ANSELbits.ANS5		/// Signal A analog selection bit
+#define	INCENC_ANS_B			ANSELbits.ANS4		/// Signal B analog selection bit
+#define	INCENC_ANSBM_A			(1 << 5)			/// Signal A analog selection bit
+#define	INCENC_ANSBM_B			(1 << 4)			/// Signal B analog selection bit
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// GLOBAL VARIABLES
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// GLOBAL PROTOTYPES
+
+/**
+ * Initializes the incremental encoder module.
+ */
+void IncEnc_Initialize( void );
+
+
+/**
+ * Gets the counter delta insce the last call, then clears it.
+ *
+ * @return
+ *		The counter delta since the last call.
+ */
+signed short IncEnc_GetDelta( void );
+
+
+/**
+ * Enables incremental encoder interrupts.
+ */
+void IncEnc_EnableInterrupts( void );
+
+
+/**
+ * Disables incremental encoder interrupts.
+ */
+void IncEnc_DisableInterrupts( void );
+
+
+/**
+ * Signal A interrupt callback.
+ */
+void IncEnc_CallbackA( void );
+
+
+/**
+ * Signal B interrupt callback.
+ */
+void IncEnc_CallbackB( void );
+
+
+#endif	/* !__INCENC_H__ */
---
+++ src/firmware/src/incenc.c
@@ -0,0 +1,100 @@
+/**
+ * Incremental encoder driver.
+ *
+ * This file contains the definitions to handle the incremental encoder.
+ *
+ * @author Andrea "TexZK" Zoppi
+ *
+ */
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// HEADERS
+
+#include "Compiler.h"
+#include "debug.h"
+#include "incenc.h"
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// LOCAL DEFINITIONS
+#pragma udata data_incenc_local
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// LOCAL VARIABLES
+#pragma udata data_incenc_local
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// GLOBAL VARIABLES
+#pragma udata data_incenc_global
+
+#pragma udata access data_incenc_global_acs
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// LOCAL PROTOTYPES
+#pragma code code_incenc_local
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// LOCAL FUNCTIONS
+#pragma code code_incenc_local
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+// GLOBAL FUNCTIONS
+#pragma code code_incenc_global
+
+void IncEnc_Initialize( void )
+{
+	// Disable peripherals
+	IncEnc_DisableInterrupts();			// Disable interrupts
+	CM1CON0bits.C1ON = 0;				// Disable Comparator 1
+	CM2CON0bits.C2ON = 0;				// Disable Comparator 2
+	
+	// Setup pins
+	INCENC_ANS_A = 1;					// Set as analog pins
+	INCENC_ANS_B = 1;
+	INCENC_TRIS_A = 1;					// Set as inputs
+	INCENC_TRIS_B = 1;
+	
+	// TODO: Setup Comparator 1
+	
+	// TODO: Setup Comparator 2
+	
+	// TODO: Enable interrupts
+}
+
+
+signed short IncEnc_GetDelta( void )
+{
+	// TODO
+}
+
+
+void IncEnc_EnableInterrupts( void )
+{
+	// TODO
+}
+
+
+void IncEnc_DisableInterrupts( void )
+{
+	// TODO
+}
+
+
+void IncEnc_CallbackA( void )
+{
+	// TODO
+}
+
+
+void IncEnc_CallbackB( void )
+{
+	// TODO
+}
+
+
+// EOF
