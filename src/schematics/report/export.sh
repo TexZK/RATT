0d1cf3d8e4405619d0cb43e7036f5a06613a3713
Andrea Zoppi <texzk@email.it>
2012-08-14 16:00:34 +0200

    Added report schematics sources
    Eagle sources of the report schematic examples were added to the
    repository.
    
    Andrea Zoppi

---
+++ src/schematics/report/export.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+project_folder=report					# Matches the Eagle subfolder name
+
+eagle_folder=UNKNOWN
+this_folder=$(pwd)
+platform=$(uname -a)
+if [ "x${platform:0:5}" = "xMINGW" ]; then
+	eagle_folder=$HOME/Documents/eagle		# Windows
+else
+	eagle_folder=$HOME/eagle			# Linux (default)
+fi
+
+source=$this_folder
+target=$eagle_folder/$project_folder
+files=$(ls -1 "$this_folder" | grep -v \.sh$)
+
+rm -rf "$target"
+mkdir -p "$target"
+echo "$files" | xargs -i cp -Rv {} "$target"
+
---
+++ src/schematics/report/import.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+project_folder=report					# Matches the Eagle subfolder name
+
+eagle_folder=UNKNOWN
+this_folder=$(pwd)
+platform=$(uname -a)
+if [ "x${platform:0:5}" = "xMINGW" ]; then
+	eagle_folder=$HOME/Documents/eagle		# Windows
+else
+	eagle_folder=$HOME/eagle			# Linux (default)
+fi
+
+source=$eagle_folder/$project_folder
+target=$this_folder
+
+files=$(ls -1 "$source" | grep -v '.*\..#.')
+echo "$files" | xargs -i cp -Rv "$source/{}" "$target"
+
